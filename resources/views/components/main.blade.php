89e64b9c279073495e568721611512d25a02d91f
HildebrandoLima <hildebrandolima16@gmail.com>
2022-10-22 11:32:05 -0300

    Refatorando rotas e adicionando arquivos internos como js/css

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -4,19 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-        body {
-            min-height: 100vh;
-            margin: auto;
-            display: flex;
-            flex-direction: column;
-            background-image: url("https://greenpng.com/wp-content/uploads/2022/09/Desenho-de-pastagem-1024x724.jpg");
-            background-repeat: no-repeat;
-            background-size: cover;
-            align-items: center;
-        }
-    </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">	
+        <script type="text/javascript" src="<?php echo asset('js/test.js')?>"></script>
+        <link rel="stylesheet" href="<?php echo asset('css/background-image.css')?>" type="text/css">
     </head>
     <body>
         <div class="container mt-3">
--- routes/web.php
+++ routes/web.php
@@ -5,34 +5,9 @@ use App\Http\Controllers\VendaController;
 use App\Http\Controllers\CaixaController;
 use App\Http\Controllers\ProdutoController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 //Route::get('/', [VendaController::class, 'index'])->name('index');
 
-Route::get('/caixa', [CaixaController::class, 'index'])->name('caixa');
-Route::get('/caixa/{codigo_barra}', [CaixaController::class, 'index'])->name('itens.listar');
-
-//  Item
-Route::prefix('item')->group(function () {
-    Route::get('/', [ItemController::class, 'index'])->name('item.listar');
-});
-
-//  Venda
-Route::prefix('venda')->group(function () {
-    //Route::get('/', [VendaController::class, 'index'])->name('venda.listar.todos');
-    //Route::get('/listar/{vendaId}', [VendaController::class, 'show'])->name('user.list.details');
-    //Route::get('/buscar/{cpf}', [VendaController::class, 'search'])->name('user.list.search');
-    //Route::put('/editar/{usuarioId}', [VendaController::class, 'update'])->name('user.edit');
-    Route::post('/salvar', [VendaController::class, 'store'])->name('venda.salvar');
-    //Route::delete('/remover/{usuarioId}', [VendaController::class, 'destroy'])->name('user.remove');
+Route::prefix('caixa')->group(function () {
+    Route::get('/', [CaixaController::class, 'index'])->name('caixa');
+    Route::get('/{codigo_barra}', [CaixaController::class, 'index'])->name('itens.listar');
 });
