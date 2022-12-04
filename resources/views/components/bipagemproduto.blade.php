72519d0e6bbfb25b8bcc503d0ec29f938b26a997
HildebrandoLima <hildebrandolima16@gmail.com>
2022-12-04 17:53:38 -0300

    Habilitando e desabilitando bipagem ao abrir/fechar caixa

--- resources/views/components/bipagemproduto.blade.php
+++ resources/views/components/bipagemproduto.blade.php
@@ -1,9 +1,16 @@
 <form action="{{ route('buscar') }}" method="post">
     @csrf
     <div class="input-group">
+        @if($status == 'Fechado')
+        <div class="form-outline">
+            <input type="number" name="codigo_barra" id="codigo_barra" placeholder="Digite o Código de Barras" class="form-control" disabled/>
+        </div>
+        <button type="submit" class="btn btn-primary" disabled>+</button>
+        @else
         <div class="form-outline">
             <input type="number" name="codigo_barra" id="codigo_barra" placeholder="Digite o Código de Barras" class="form-control" />
         </div>
         <button type="submit" class="btn btn-primary">+</button>
+        @endif
       </div>
 </form>
